ba68f98eadb8e327c1c36b6bc1706eadf776214d
Kandy-Hamisi <hkandy2000@gmail.com>
2022-01-20 22:35:08 +0300

    fixed chair design

---
+++ bus-booking/script.js
@@ -0,0 +1,12 @@
+const busContainer = document.querySelector(".busWrapper");
+
+
+let count = 1;
+let totalSeats = 43;
+
+while (count <= totalSeats) {
+    // console.log(count);
+    busContainer.innerHTML = "hello";
+    count ++;
+}
+
