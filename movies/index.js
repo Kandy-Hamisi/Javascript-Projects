2e9c65bb38efd249d054a8e3452fbe0b52da51cb
Kandy-Hamisi <hkandy2000@gmail.com>
2022-02-24 10:31:39 +0300

    Update: returned fetching API

--- movies/index.js
+++ movies/index.js
@@ -21,48 +21,48 @@ hamburgerBtn.onclick = (e) => {
 }
 
 
-// async function fetchMovie() {
+async function fetchMovie() {
     
-//     url = "https://imdb-api.com/en/API/MostPopularMovies/k_sopto3ua";
-//     try {
-//         let response = await fetch(url);
-//         let data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+    url = "https://imdb-api.com/en/API/MostPopularMovies/k_sopto3ua";
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-// // display the items in a card
+// display the items in a card
 
-// async function renderMovie() {
-//     let movies = await fetchMovie();
-//     let html = "";
-//     console.log(typeof(movies));
-//     console.log(movies);
+async function renderMovie() {
+    let movies = await fetchMovie();
+    let html = "";
+    console.log(typeof(movies));
+    console.log(movies);
     
-//     let movieArray = movies.items;
-//     let shortMovieList = movieArray.slice(0, 10);
+    let movieArray = movies.items;
+    let shortMovieList = movieArray.slice(0, 10);
 
 
-//     shortMovieList.forEach(element => {
-//         // console.log(element);
-//         let card = 
-//         `<div class="card">
-//             <img class="images" src="${element.image}" />
-//             <h5 class="title title-font">${element.title}</h5>
-//         </div>
-//         `;
-//         html += card;
-//     });
-    
-
-//     let container = document.querySelector('.container');
-//     container.innerHTML = html;
+    shortMovieList.forEach(element => {
+        // console.log(element);
+        let card = 
+        `<div class="card">
+            <img class="images" src="${element.image}" />
+            <h5 class="title title-font">${element.title}</h5>
+        </div>
+        `;
+        html += card;
+    });
     
 
-// }
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+    
 
-// renderMovie();
+}
+
+renderMovie();
 
 
